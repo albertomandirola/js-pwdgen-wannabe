35a02ab11e1524f6921481edef8f56c88f458500
albertomandirola <albymandi00@gmail.com>
2023-11-17 15:09:22 +0100

    push prima soluzione con writeln e senza id

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 //Prima soluzione
 let nome = prompt('qual e il tuo nome?');
-console.log(nome)
+console.log(nome);
 let cognome = prompt('qual e il tuo cognome?');
-console.log(cognome)
+console.log(cognome);
 let colore = prompt('qual e il tuo colore preferito ?');
-console.log(colore)
+console.log(colore);
+let password = `${nome}${cognome}${colore}${23}`;
+document.writeln(password);
