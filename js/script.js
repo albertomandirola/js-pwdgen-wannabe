065fba8ff46486a0bdfb314d416031ea00ba4f81
albertomandirola <albymandi00@gmail.com>
2023-11-17 15:05:45 +0100

    add second and third request

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
+//Prima soluzione
 let nome = prompt('qual e il tuo nome?');
 console.log(nome)
+let cognome = prompt('qual e il tuo cognome?');
+console.log(cognome)
+let colore = prompt('qual e il tuo colore preferito ?');
+console.log(colore)
